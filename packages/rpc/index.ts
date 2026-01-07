773e86ac6b04e8d50f7e490c9a805e9d879ea4ab
andresmarpz <andresmarpz@gmail.com>
2026-01-07 10:37:56 -0300

    feat: add @sandcastle/rpc package with effect rpc schema and handlers

---
+++ packages/rpc/index.ts
@@ -0,0 +1,4 @@
+export * from "./src/types"
+export * from "./src/errors"
+export { WorktreeRpc } from "./src/schema"
+export { WorktreeRpcHandlers, WorktreeRpcHandlersLive } from "./src/handlers"
---
+++ packages/rpc/src/schema.ts
@@ -0,0 +1,45 @@
+import { Rpc, RpcGroup } from "@effect/rpc"
+import { Schema } from "effect"
+import { WorktreeInfo, CreateWorktreeOptions, RemoveWorktreeOptions } from "./types"
+import {
+  GitCommandRpcError,
+  WorktreeExistsRpcError,
+  WorktreeNotFoundRpcError,
+  BranchExistsRpcError,
+} from "./errors"
+
+export class WorktreeRpc extends RpcGroup.make(
+  Rpc.make("worktree.list", {
+    payload: { repoPath: Schema.String },
+    success: Schema.Array(WorktreeInfo),
+    error: GitCommandRpcError,
+  }),
+
+  Rpc.make("worktree.get", {
+    payload: { repoPath: Schema.String, worktreePath: Schema.String },
+    success: WorktreeInfo,
+    error: Schema.Union(GitCommandRpcError, WorktreeNotFoundRpcError),
+  }),
+
+  Rpc.make("worktree.create", {
+    payload: CreateWorktreeOptions,
+    success: WorktreeInfo,
+    error: Schema.Union(
+      GitCommandRpcError,
+      WorktreeExistsRpcError,
+      BranchExistsRpcError
+    ),
+  }),
+
+  Rpc.make("worktree.remove", {
+    payload: RemoveWorktreeOptions,
+    success: Schema.Void,
+    error: Schema.Union(GitCommandRpcError, WorktreeNotFoundRpcError),
+  }),
+
+  Rpc.make("worktree.prune", {
+    payload: { repoPath: Schema.String },
+    success: Schema.Void,
+    error: GitCommandRpcError,
+  })
+) {}
