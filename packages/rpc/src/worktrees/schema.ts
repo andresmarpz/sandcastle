0cdf23043bcdfda40e0086fe3c1b5d2986560bd7
andresmarpz <andresmarpz@gmail.com>
2026-01-08 18:48:29 -0300

    feat(rpc): add worktree RPC schema and errors

---
+++ packages/rpc/src/worktrees/schema.ts
@@ -0,0 +1,63 @@
+import { Rpc, RpcGroup } from "@effect/rpc";
+import { Schema } from "effect";
+import { Worktree, CreateWorktreeInput, UpdateWorktreeInput } from "@sandcastle/storage/entities";
+import { DatabaseRpcError, ForeignKeyViolationRpcError } from "../common/errors";
+import { WorktreeNotFoundRpcError, WorktreePathExistsRpcError, GitOperationRpcError } from "./errors";
+
+export class WorktreeRpc extends RpcGroup.make(
+  Rpc.make("worktree.list", {
+    payload: {},
+    success: Schema.Array(Worktree),
+    error: DatabaseRpcError
+  }),
+
+  Rpc.make("worktree.listByRepository", {
+    payload: { repositoryId: Schema.String },
+    success: Schema.Array(Worktree),
+    error: DatabaseRpcError
+  }),
+
+  Rpc.make("worktree.get", {
+    payload: { id: Schema.String },
+    success: Worktree,
+    error: Schema.Union(WorktreeNotFoundRpcError, DatabaseRpcError)
+  }),
+
+  Rpc.make("worktree.getByPath", {
+    payload: { path: Schema.String },
+    success: Worktree,
+    error: Schema.Union(WorktreeNotFoundRpcError, DatabaseRpcError)
+  }),
+
+  Rpc.make("worktree.create", {
+    payload: CreateWorktreeInput,
+    success: Worktree,
+    error: Schema.Union(
+      WorktreePathExistsRpcError,
+      ForeignKeyViolationRpcError,
+      GitOperationRpcError,
+      DatabaseRpcError
+    )
+  }),
+
+  Rpc.make("worktree.update", {
+    payload: Schema.Struct({
+      id: Schema.String,
+      input: UpdateWorktreeInput
+    }),
+    success: Worktree,
+    error: Schema.Union(WorktreeNotFoundRpcError, DatabaseRpcError)
+  }),
+
+  Rpc.make("worktree.delete", {
+    payload: { id: Schema.String },
+    success: Schema.Void,
+    error: Schema.Union(WorktreeNotFoundRpcError, GitOperationRpcError, DatabaseRpcError)
+  }),
+
+  Rpc.make("worktree.touch", {
+    payload: { id: Schema.String },
+    success: Schema.Void,
+    error: Schema.Union(WorktreeNotFoundRpcError, DatabaseRpcError)
+  })
+) {}
